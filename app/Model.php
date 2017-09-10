145c5b51ed5971aad1cc1a5f2502357eec586aff
Zhou Qingwu <zhqingwu@gmail.com>
2017-09-10 11:36:01 +0800

    Model guarded wrong spelling

--- app/Model.php
+++ app/Model.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class Model extends Eloquent
 {
 	// protected $fillable = ['body', 'task_id'];
-	protected $gurded = [];
+	protected $guarded = [];
 }
