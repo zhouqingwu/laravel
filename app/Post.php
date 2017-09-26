241d07d7ab5c569e6298ed674ea8f60fa95cab39
Zhou Qingwu <zhqingwu@gmail.com>
2017-09-26 14:30:28 +0800

    Add Tag

--- app/Post.php
+++ app/Post.php
@@ -16,6 +16,11 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }    
 
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }    
+
     public function addDiscuss($body)
     {
         // Discuss::create([
